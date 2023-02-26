5872aa068f0fa9ef27610b3fcc2f64b4510b3fbe
hadisptr <anandahadisaputra@gmail.com>
2023-02-26 22:07:39 +0700

    Add project files.

---
+++ People/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace People.Models;
+
+[Table("people")]
+public class Person
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [Unique, MaxLength(100)]
+    public string Name { get; set; }
+}
